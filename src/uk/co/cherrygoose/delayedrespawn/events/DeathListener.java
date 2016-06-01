3e85f58e4002064efa902ab51f233103751334cc
Ross-Gosling <rossjgosling@gmail.com>
2016-06-01 11:03:24 +0100

    Removed Debug Messages
    Removed a couple of irrelevant debug messages

--- src/uk/co/cherrygoose/delayedrespawn/events/DeathListener.java
+++ src/uk/co/cherrygoose/delayedrespawn/events/DeathListener.java
@@ -1,12 +1,10 @@
 package uk.co.cherrygoose.delayedrespawn.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-import uk.co.cherrygoose.delayedrespawn.Main;
 import uk.co.cherrygoose.delayedrespawn.systems.DeathCooldown;
 
 public class DeathListener implements Listener 
@@ -14,9 +12,6 @@ public class DeathListener implements Listener
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerDeath(PlayerDeathEvent event)
 	{
-    	// Logs to console
-    	Bukkit.getLogger().info("["+Main.pluginName+"] onPlayerDeath called");
-		
 		// Adds death cooldown to player
 		DeathCooldown.add(event.getEntity());
 	}
--- src/uk/co/cherrygoose/delayedrespawn/events/RespawnListener.java
+++ src/uk/co/cherrygoose/delayedrespawn/events/RespawnListener.java
@@ -1,13 +1,11 @@
 package uk.co.cherrygoose.delayedrespawn.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
-import uk.co.cherrygoose.delayedrespawn.Main;
 import uk.co.cherrygoose.delayedrespawn.systems.DeathCooldown;
 
 public class RespawnListener implements Listener 
@@ -15,15 +13,12 @@ public class RespawnListener implements Listener
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerRespawn(PlayerRespawnEvent event) 
 	{
-    	// Logs to console
-    	Bukkit.getLogger().info("["+Main.pluginName+"] onPlayerRespawn called");
-		
 		// Gets Player
 		Player player = event.getPlayer();
 
 		// If player has a death cooldown active
 		if(DeathCooldown.get(player) > 0)
-		{
+		{			
 			// Kicks player
 			player.kickPlayer("Respawn available in " + DeathCooldown.toReadable(DeathCooldown.get(player)));
 		}
