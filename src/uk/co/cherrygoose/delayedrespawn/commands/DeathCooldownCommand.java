b02dfd46174d6deb9b4401ff07269acc9b69ecd0
Ross-Gosling <rossjgosling@gmail.com>
2016-06-01 13:27:49 +0100

    Added "/delayedrespawn set" Command

--- src/uk/co/cherrygoose/delayedrespawn/commands/DeathCooldownCommand.java
+++ src/uk/co/cherrygoose/delayedrespawn/commands/DeathCooldownCommand.java
@@ -34,7 +34,7 @@ public class DeathCooldownCommand {
 				// If there are arguments
 				if(args.length != 0)
 				{
-					// If first arg is "set"
+					// If first arg is "clear"
 					if(args[0].equalsIgnoreCase("clear"))
 					{
 						// If second arg exists
@@ -54,7 +54,7 @@ public class DeathCooldownCommand {
 							}
 							
 							// Logs to console
-							Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /deathcooldown clear " + args[1]);
+							Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /delayedrespawn clear " + args[1]);
 							
 							// If arg is a player name
 							if(coolingPlayerUUIDs.contains(Bukkit.getServer().getOfflinePlayer(args[1]).getUniqueId()))
@@ -81,20 +81,69 @@ public class DeathCooldownCommand {
 						else 
 						{
 							// Logs to console
-							Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /deathcooldown clear " + args[0]);
+							Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /delayedrespawn clear " + args[0]);
 							
 							// Sends player feedback
-							player.sendMessage(ChatColor.RED + "Usage: /deathcooldown clear <player>");
+							player.sendMessage(ChatColor.RED + "Usage: /delayedrespawn clear <player>");
+							return true;
+						}
+					}
+					// If first arg is "set"
+					else if(args[0].equalsIgnoreCase("set")) 
+					{
+						// If second arg exists
+						if(args.length > 1)
+						{							
+							// Logs to console
+							Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /delayedrespawn set " + args[1]);
+							
+							try
+							{
+								// If arg is a positive double
+								if(Double.parseDouble(args[1]) >= 0.0)
+								{
+									// Sets new delay in config
+									Main.config().set("Delay", Double.parseDouble(args[1]));
+
+									// Sends player feedback
+									player.sendMessage(ChatColor.WHITE + "New respawn delay set: " + Double.parseDouble(args[1]) + " hours");
+				
+							    	// Logs to console
+							    	Bukkit.getLogger().info("["+Main.pluginName+"] New respawn delay set: " + Double.parseDouble(args[1]) + " hours");
+								}
+								// Invalid duration
+								else
+								{
+									// Sends player feedback
+									player.sendMessage(ChatColor.RED + "Invalid delay '" + args[1] + "'");
+									return true;
+								}
+							}
+							catch (NumberFormatException e)
+							{
+								// Sends player feedback
+								player.sendMessage(ChatColor.RED + "Invalid number format '" + args[1] + "'");
+								return true;
+							}
+						}
+						else 
+						{
+							// Logs to console
+							Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /delayedrespawn set " + args[0]);
+							
+							// Sends player feedback
+							player.sendMessage(ChatColor.RED + "Usage: /delayedrespawn set <hours>");
 							return true;
 						}
 					}
 					else 
 					{
 						// Logs to console
-						Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /colour " + args[0]);
+						Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /delayedrespawn " + args[0]);
 						
 						// Sends player feedback
-						player.sendMessage(ChatColor.RED + "Usage: /deathcooldown clear <player>");
+						player.sendMessage(ChatColor.RED + "Usage: /delayedrespawn <clear/set>");
+						
 						return true;
 					}
 				}
@@ -102,14 +151,15 @@ public class DeathCooldownCommand {
 				else
 				{
 					// Logs to console
-					Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /deathcooldown");
+					Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /delayedrespawn");
 					return true;
 				}
 			}
+			
 			else
 			{
 				// Logs to console
-				Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /deathcooldown");
+				Bukkit.getLogger().info("["+Main.pluginName+"] " + player.getName() + ": /delayedrespawn");
 
 				// Sends player feedback
 				player.sendMessage(ChatColor.RED + "You must be an Operator to use this command");
@@ -123,12 +173,15 @@ public class DeathCooldownCommand {
 			// If there are arguments
 			if(args.length != 0)
 			{
-				// If first arg is "set"
+				// If first arg is "clear"
 				if(args[0].equalsIgnoreCase("clear"))
 				{
 					// If second arg exists
 					if(args.length > 1)
-					{
+					{				
+						// Logs to console
+						Bukkit.getLogger().info("["+Main.pluginName+"] /delayedrespawn clear " + args[1]);
+						
 						// Creates list for available players in cooldown
 						ArrayList<UUID> coolingPlayerUUIDs = new ArrayList<UUID>();
 						// For all players
@@ -164,14 +217,56 @@ public class DeathCooldownCommand {
 					else 
 					{
 						// Sends console feedback
-						Bukkit.getLogger().info("["+Main.pluginName+"] " + "Usage: /deathcooldown clear <player>");
+						Bukkit.getLogger().info("["+Main.pluginName+"] " + "Usage: /delayedrespawn clear <player>");
+						return true;
+					}
+				}
+				// If first arg is "set"
+				else if(args[0].equalsIgnoreCase("set")) 
+				{
+					// If second arg exists
+					if(args.length > 1)
+					{						
+						// Logs to console
+						Bukkit.getLogger().info("["+Main.pluginName+"] /delayedrespawn set " + args[1]);
+						
+						try
+						{
+							// If arg is a positive double
+							if(Double.parseDouble(args[1]) >= 0.0)
+							{
+								// Sets new delay in config
+								Main.config().set("Delay", Double.parseDouble(args[1]));
+
+						    	// Logs to console
+						    	Bukkit.getLogger().info("["+Main.pluginName+"] New respawn delay set: " + Double.parseDouble(args[1]) + " hours");
+							}
+							// Invalid duration
+							else
+							{
+								// Sends console feedback
+								Bukkit.getLogger().info("["+Main.pluginName+"] Invalid delay '" + args[1] + "'");
+								return true;
+							}
+						}
+						catch (NumberFormatException e)
+						{
+							// Sends console feedback
+							Bukkit.getLogger().info("["+Main.pluginName+"] Invalid number format '" + args[1] + "'");
+							return true;
+						}
+					}
+					else 
+					{
+						// Sends player feedback
+						Bukkit.getLogger().info("["+Main.pluginName+"] " + "Usage: /delayedrespawn set <hours>");
 						return true;
 					}
 				}
 				else 
 				{
 					// Sends console feedback
-					Bukkit.getLogger().info("["+Main.pluginName+"] " + "Usage: /deathcooldown clear <player>");
+					Bukkit.getLogger().info("["+Main.pluginName+"] " + "Usage: /delayedrespawn <clear/set>");
 					return true;
 				}
 			}
@@ -179,7 +274,7 @@ public class DeathCooldownCommand {
 			else
 			{
 				// Logs to console
-				Bukkit.getLogger().info("["+Main.pluginName+"] " + "Usage: /deathcooldown clear <player>");
+				Bukkit.getLogger().info("["+Main.pluginName+"] " + "Usage: /delayedrespawn <clear/set>");
 				return true;
 			}
 		}
@@ -195,101 +290,105 @@ public class DeathCooldownCommand {
 	
 	public static List<String> onTabComplete(CommandSender sender, Command cmd, String commandLabel, String[] args) 
 	{
-		// If has 1 argument
-		if(args.length == 1)
+		// If sender is a player
+		if(sender instanceof Player) 
 		{
-			// Declares array for appropriate arguments to return
-			ArrayList<String> returnedArgs = new ArrayList<String>();
+			Player player = (Player) sender;
 			
-			// Declares array of valid arguments
-			ArrayList<String> validArgs = new ArrayList<String>();
-			// Defines valid args
-			validArgs.add("clear");
-						
-			// If arg is not empty
-			if(!args[0].toString().equals(""))
+			// If has 1 argument
+			if(args.length == 1)
 			{
-				// For all validArgs
-				for (String validArg : validArgs)
+				// Declares array for appropriate arguments to return
+				ArrayList<String> returnedArgs = new ArrayList<String>();
+				
+				// Declares array of valid arguments
+				ArrayList<String> validArgs = new ArrayList<String>();
+				// Defines valid args
+				validArgs.add("clear");
+				if(player.isOp()) { validArgs.add("set"); }
+				
+				// If arg is not empty
+				if(!args[0].toString().equals(""))
 				{
-					// If validArg starts with argument
-					if(validArg.toLowerCase().startsWith(args[0].toLowerCase()))
+					// For all validArgs
+					for (String validArg : validArgs)
 					{
-						// Adds validArg to list
-						returnedArgs.add(validArg);
+						// If validArg starts with argument
+						if(validArg.toLowerCase().startsWith(args[0].toLowerCase()))
+						{
+							// Adds validArg to list
+							returnedArgs.add(validArg);
+						}
 					}
 				}
-			}
-			// If arg is empty
-			else
-			{
-				// For all validArgs
-				for (String validArg : validArgs)
+				// If arg is empty
+				else
 				{
-					// If validArg is not null
-					if(validArg != null)
+					// For all validArgs
+					for (String validArg : validArgs)
 					{
-						// Adds validArg to list
-						returnedArgs.add(validArg);
+						// If validArg is not null
+						if(validArg != null)
+						{
+							// Adds validArg to list
+							returnedArgs.add(validArg);
+						}
 					}
 				}
+				
+				// Sorts list into alphabetical order
+				Collections.sort(returnedArgs);
+				
+				// Returns list
+				return returnedArgs;
 			}
-			
-			// Sorts list into alphabetical order
-			Collections.sort(returnedArgs);
-			
-			// Returns list
-			return returnedArgs;
-		}
-		// If has "set" arg and another
-		else if (args[0].equalsIgnoreCase("set") && args.length == 2)
-		{
-			// Declares array for appropriate player names
-			ArrayList<String> returnedPlayerNames = new ArrayList<String>();
-			
-			returnedPlayerNames.add("AIDS");
-			return returnedPlayerNames;
-			
-			// If arg is not empty
-			/*if(!args[1].toString().equals(""))
+			// If has "clear" arg and another
+			else if (args[0].equalsIgnoreCase("clear") && args.length == 2)
 			{
-				// For all players
-				for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers())
+				// Declares array for appropriate player names
+				ArrayList<String> returnedPlayerNames = new ArrayList<String>();
+				
+				// If arg is not empty
+				if(!args[1].toString().equals(""))
 				{
-					// If player has death cooldown
-					if(DeathCooldown.get(offlinePlayer) > 0)
+					// For all players
+					for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers())
 					{
-						// If player name starts with argument
-						if(offlinePlayer.getName().toLowerCase().startsWith(args[1].toLowerCase()))
+						// If player has death cooldown
+						if(DeathCooldown.get(offlinePlayer) > 0)
+						{
+							// If player name starts with argument
+							if(offlinePlayer.getName().toLowerCase().startsWith(args[1].toLowerCase()))
+							{
+								// Adds player name to list
+								returnedPlayerNames.add(offlinePlayer.getName());
+							}
+						}
+					}
+				}
+				// If arg is empty
+				else
+				{
+					// For all players
+					for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers())
+					{
+						// If player has death cooldown
+						if(DeathCooldown.get(offlinePlayer) > 0)
 						{
 							// Adds player name to list
 							returnedPlayerNames.add(offlinePlayer.getName());
 						}
 					}
 				}
+				
+				// Sorts list into alphabetical order
+				Collections.sort(returnedPlayerNames);
+				
+				// Returns list
+				return returnedPlayerNames;
 			}
-			// If arg is empty
-			else
-			{
-				// For all players
-				for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers())
-				{
-					// If player has death cooldown
-					if(DeathCooldown.get(offlinePlayer) > 0)
-					{
-						// Adds player name to list
-						returnedPlayerNames.add(offlinePlayer.getName());
-					}
-				}
-			}
-			
-			// Sorts list into alphabetical order
-			Collections.sort(returnedPlayerNames);
-			
-			// Returns list
-			return returnedPlayerNames;*/
 		}
-		
+			
 		return null;
 	}
 }
